a75bef9d9c7a479e3539d8405b3495151d5938c9
DanyloKozlovskyi <danilokozlovskij@gmail.com>
2025-07-05 23:45:27 +0300

    created categoryrepository, categoryservice, categoriescontroller

--- src/BulletinBoard.Api/Controllers/CategoriesController.cs
+++ src/BulletinBoard.Api/Controllers/CategoriesController.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulletinBoard.Application.Categories;
+using BulletinBoard.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulletinBoard.Api.Controllers;
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class CategoriesController : ControllerBase
 {
+	private readonly ICategoryService _service;
+
+	public CategoriesController(ICategoryService service)
+	{
+		_service = service;
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Category>>> GetAll()
+	{
+		var categories = await _service.ListAsync();
+		return Ok(categories);
+	}
+
+	[HttpGet("{id:int}")]
+	public async Task<ActionResult<Category>> GetById(int id)
+	{
+		var cat = await _service.GetByIdAsync(id);
+		if (cat is null) return NotFound();
+		return Ok(cat);
+	}
 }
